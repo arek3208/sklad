1fd94879f4aa27d1e0ffe86ca7542ed84e3feece
Arkadiusz Kumor <arek3208@gmail.com>
2020-05-12 16:37:14 +0200

    kończenie zamówień, zabezpieczenia

--- Controllers/AddressesController.cs
+++ Controllers/AddressesController.cs
@@ -31,25 +31,6 @@ namespace sklad.Controllers
             return View(await addresses.ToListAsync());
         }
 
-        // GET: Addresses/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var address = await _context.Address
-                .Include(a => a.ApplicationUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (address == null)
-            {
-                return NotFound();
-            }
-
-            return View(address);
-        }
-
         // GET: Addresses/Create
         public IActionResult Create(bool Cart = false)
         {
@@ -73,59 +54,6 @@ namespace sklad.Controllers
                 return RedirectToAction(nameof(Index));
             }
             ViewBag.Cart = Cart;
-            return View(address);
-        }
-
-        // GET: Addresses/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var address = await _context.Address.FindAsync(id);
-            if (address == null)
-            {
-                return NotFound();
-            }
-            return View(address);
-        }
-
-        // POST: Addresses/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Street,BuildingNo,UnitNo,PostalCode,City")] Address address)
-        {
-            if (id != address.Id)
-            {
-                return NotFound();
-            }
-
-            address.ApplicationUser = await _userManager.GetUserAsync(User);
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(address);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AddressExists(address.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
             return View(address);
         }
 
--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -96,7 +96,7 @@ namespace sklad.Controllers
 		public async Task<IActionResult> MyOrders()
 		{
 			var user = await _userManager.GetUserAsync(User);
-			var orders = _context.Order.Include(o => o.Client).Include(o => o.Driver).Include(o => o.Address).Where(o => o.ClientId == user.Id);
+			var orders = _context.Order.Include(o => o.Client).Include(o => o.Driver).Include(o => o.Address).Include(o => o.OrderItems).ThenInclude(o => o.Item).Where(o => o.ClientId == user.Id);
 			return View(await orders.ToListAsync());
 		}
 
@@ -106,7 +106,7 @@ namespace sklad.Controllers
 			var user = await _userManager.GetUserAsync(User);
 			var orders = _context.Order.Include(o => o.Client).Include(o => o.OrderItems).ThenInclude(o => o.Item).Include(o => o.Address);
 			var availableOrders = orders.Where(o => o.Driver == null).ToList();
-			var myOrders = orders.Where(o => o.DriverId == user.Id).ToList();
+			var myOrders = orders.Where(o => o.DriverId == user.Id && !o.RealizationDate.HasValue).ToList();
 			return View(new Tuple<List<Order>, List<Order>>(myOrders, availableOrders));
 		}
 
@@ -114,7 +114,7 @@ namespace sklad.Controllers
 		public async Task<IActionResult> DriverFinishedOrders()
 		{
 			var user = await _userManager.GetUserAsync(User);
-			var orders = _context.Order.Include(o => o.Client).Include(o => o.Driver).Include(o => o.Address).Where(o => o.Driver.Id == user.Id && o.RealizationDate.HasValue);
+			var orders = _context.Order.Include(o => o.Client).Include(o => o.Driver).Include(o => o.Address).Include(o => o.OrderItems).ThenInclude(o => o.Item).Where(o => o.Driver.Id == user.Id && o.RealizationDate.HasValue);
 			return View(orders);
 		}
 
@@ -124,7 +124,7 @@ namespace sklad.Controllers
 			var order = _context.Order.Find(id);
 			if(order.Driver != null)
 			{
-				return BadRequest();
+				return Unauthorized();
 			}
 			order.Driver = await _userManager.GetUserAsync(User);
 			_context.Update(order);
@@ -132,95 +132,22 @@ namespace sklad.Controllers
 			return RedirectToAction("AvailableOrders");
 		}
 
-		// GET: Orders/Edit/5
-		public async Task<IActionResult> Edit(int? id)
+		[Authorize(Roles = "Admin, Driver")]
+		public IActionResult FinishOrder(int id)
 		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-
-			var order = await _context.Order.FindAsync(id);
+			var order = _context.Order.Find(id);
 			if (order == null)
 			{
 				return NotFound();
 			}
-			ViewData["ClientId"] = new SelectList(_context.Users, "Id", "Id", order.ClientId);
-			ViewData["DriverId"] = new SelectList(_context.Users, "Id", "Id", order.DriverId);
-			return View(order);
-		}
-
-		// POST: Orders/Edit/5
-		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(int id, [Bind("Id,OrderDate,RealizationDate,ClientId,DriverId")] Order order)
-		{
-			if (id != order.Id)
+			if(order.RealizationDate.HasValue)
 			{
-				return NotFound();
+				return Unauthorized();
 			}
-
-			if (ModelState.IsValid)
-			{
-				try
-				{
-					_context.Update(order);
-					await _context.SaveChangesAsync();
-				}
-				catch (DbUpdateConcurrencyException)
-				{
-					if (!OrderExists(order.Id))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
-				}
-				return RedirectToAction(nameof(Index));
-			}
-			ViewData["ClientId"] = new SelectList(_context.Users, "Id", "Id", order.ClientId);
-			ViewData["DriverId"] = new SelectList(_context.Users, "Id", "Id", order.DriverId);
-			return View(order);
-		}
-
-		// GET: Orders/Delete/5
-		public async Task<IActionResult> Delete(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-
-			var order = await _context.Order
-				.Include(o => o.Client)
-				.Include(o => o.Driver)
-				.FirstOrDefaultAsync(m => m.Id == id);
-			if (order == null)
-			{
-				return NotFound();
-			}
-
-			return View(order);
-		}
-
-		// POST: Orders/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> DeleteConfirmed(int id)
-		{
-			var order = await _context.Order.FindAsync(id);
-			_context.Order.Remove(order);
-			await _context.SaveChangesAsync();
-			return RedirectToAction(nameof(Index));
-		}
-
-		private bool OrderExists(int id)
-		{
-			return _context.Order.Any(e => e.Id == id);
+			order.RealizationDate = DateTime.Now;
+			_context.Update(order);
+			_context.SaveChanges();
+			return RedirectToAction("AvailableOrders");
 		}
 	}
 }
